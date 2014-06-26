bb3e88c9db4e2e25c7908d7fe722167b3b3aedd6
= <=>
2014-06-26 01:01:44 +0000

    haven't committed in too long. improved many things

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php get_header(); ?>
 
 	<main role="main">
+        <section class='slider'>
+            <?php echo do_shortcode( '[advps-slideshow optset="1"]' ); ?>
+        </section>
 		<!-- section -->
-		<section>
+		<section class='latest'>
 
 			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
 
 			<?php get_template_part('loop'); ?>
-
-			<?php get_template_part('pagination'); ?>
 
 		</section>
 		<!-- /section -->
--- single.php
+++ single.php
@@ -9,15 +9,17 @@
 		<!-- article -->
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-            <?php if ( get_post_meta( get_the_ID(), 'video', true ) ) : ?>
-                <iframe width="980" height="551" src="//www.youtube.com/embed/<?php echo get_post_meta( get_the_ID(), 'video', true ); ?>?modestbranding=1&controls=2&rel=0&iv_load_policy=3&showinfo=0" frameborder="0" allowfullscreen></iframe>
-			<?php elseif ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+            <?php if ( get_post_meta( get_the_ID(), '_tern_wp_youtube_video', true ) ) {
+                $videoID = get_post_meta( get_the_ID(), '_tern_wp_youtube_video', true );
+            ?>
+                <iframe width="980" height="551" src="//www.youtube.com/embed/<?php echo $videoID; ?>?modestbranding=1&controls=2&rel=0&iv_load_policy=3&showinfo=0" frameborder="0" allowfullscreen></iframe>
+			<?php } else if ( has_post_thumbnail()) { // Check if Thumbnail exists ?>
                 <!-- post thumbnail -->
 				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
 				</a>
                 <!-- /post thumbnail -->
-			<?php endif; ?>
+			<?php } ?>
 
 			<!-- post title -->
 			<h1>
